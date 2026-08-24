571c64b9dc8a065135e2248043e9c981ba07280a
Cody Guillot <80494215+woahdy@users.noreply.github.com>
2026-08-23 22:26:17 -0500

    feat: export validation service

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './slp/types.js';
 export * from './slp/parser.js';
 export * from './slp/validator.js';
 export * from './bch/adapter.js';
+export * from './app/validation-service.js';
